8fbd541359c3aef97dc2d002472ab062a5ea7ea6
WLucasFranklin <wlucasfranklin@gmail.com>
2026-07-30 14:22:08 +0000

    Threading added to test_worker.py

--- app/test_worker.py
+++ app/test_worker.py
@@ -1,3 +1,22 @@
 from worker import process_employee
+from threading import Thread
 
-process_employee("60001")
+
+
+
+def run_employee_worker(employee_number):
+    thread = Thread(
+        target=process_employee,
+        args=(employee_number,),
+        daemon=False,
+        name=f"employee-{employee_number}",
+    )
+
+    thread.start()
+    return thread
+
+#run_employee_worker("60001")
+run_employee_worker("60002")
+#run_employee_worker("60003")
+
+
