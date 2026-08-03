b548451e1289e1901fd52f57c623dc526fc3b43d
WLucasFranklin <wlucasfranklin@gmail.com>
2026-08-03 20:52:01 +0000

    Bug Fix: Trailing Space

--- terraform-end-user/vpc.tf
+++ terraform-end-user/vpc.tf
@@ -3,7 +3,7 @@ data "aws_vpc" "team02" {
 }
 
 data "aws_subnet" "team02_public" {
-  id = "subnet-07ee0dd0e0342e11f "
+  id = "subnet-07ee0dd0e0342e11f"
 }
 
 resource "aws_internet_gateway" "team02" {
