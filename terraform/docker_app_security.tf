c7c39082d8f39cd09b5d03bc623ca8d96ee853a3
WLucasFranklin <wlucasfranklin@gmail.com>
2026-08-03 18:42:40 +0000

    Bug Fix: RM docker_app_security

--- terraform/docker_app_security.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_security_group_rule" "docker_app_ingress" {
-  description       = "Allow access to the Flask Docker application"
-  type              = "ingress"
-  from_port         = 5000
-  to_port           = 5000
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.frontend.id
-}
