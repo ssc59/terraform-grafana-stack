302fae8f3e6ecbe178f98666a70f67ca67f9e949
WLucasFranklin <wlucasfranklin@gmail.com>
2026-08-03 18:17:45 +0000

    Bug Fix: Open port 5000 by default

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -27,6 +27,14 @@ resource "aws_security_group" "frontend" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "HR Portal"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
